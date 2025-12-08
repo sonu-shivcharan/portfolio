440c6c4e08355409e11ce58f9aa680db4945495b
sonu-shivcharan <sonushivcharan@outlook.com>
2025-12-08 22:10:50 +0530

    create icons.ts

---
+++ lib/icons.ts
@@ -0,0 +1,47 @@
+import {
+  SiEducative,
+  SiGit,
+  SiGithub,
+  SiNextdotjs,
+  SiNodedotjs,
+  SiReact,
+  SiMongodb,
+  SiTailwindcss,
+  SiTypescript,
+  SiVercel,
+  SiJavascript,
+  SiPostman,
+  SiReaddotcv,
+} from "react-icons/si";
+
+import {
+  FaLinkedin,
+  FaPaperPlane,
+  FaXTwitter,
+  FaEnvelope,
+} from "react-icons/fa6";
+import { MapPin } from "lucide-react";
+import { FaGraduationCap } from "react-icons/fa";
+
+export const icons = {
+  educative: SiEducative,
+  git: SiGit,
+  github: SiGithub,
+  nextjs: SiNextdotjs,
+  nodejs: SiNodedotjs,
+  reactjs: SiReact,
+  mongodb: SiMongodb,
+  tailwindcss: SiTailwindcss,
+  typescript: SiTypescript,
+  vercel: SiVercel,
+  javascript: SiJavascript,
+  postman: SiPostman,
+
+  linkedin: FaLinkedin,
+  contact: FaPaperPlane,
+  xtwitter: FaXTwitter,
+  envelope: FaEnvelope,
+  resume: SiReaddotcv,
+  location: MapPin,
+  graduation: FaGraduationCap,
+};
