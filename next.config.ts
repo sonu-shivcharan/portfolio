b547d0ed53cb413abe15da8e69a666f0545a65af
sonu-shivcharan <sonushivcharan@outlook.com>
2026-01-03 19:45:52 +0530

    add remote patterns for images

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import withMDX from "@next/mdx";
 
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.scdn.co",
+      },
+    ],
+  },
   // Add other Next.js config here
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
 };
