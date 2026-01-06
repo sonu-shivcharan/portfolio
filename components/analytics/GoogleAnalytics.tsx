8b698273ed31270204aa89780c9a0c1bffb69ad2
sonu-shivcharan <sonushivcharan@outlook.com>
2026-01-06 19:55:44 +0530

    disable analytics init in dev mode

--- components/analytics/GoogleAnalytics.tsx
+++ components/analytics/GoogleAnalytics.tsx
@@ -5,14 +5,12 @@ import { useEffect } from "react";
 
 export default function GoogleAnalytics() {
   const pathname = usePathname();
-
   useEffect(() => {
-    if (!window.gtag) return;
+    if (process.env.NODE_ENV === "development" || !window.gtag) return;
 
     window.gtag("config", process.env.NEXT_PUBLIC_GA_ID!, {
       page_path: pathname,
     });
-    console.log("window.gtag", window.gtag);
   }, [pathname]);
 
   return null;
