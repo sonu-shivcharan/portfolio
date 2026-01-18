162418a4f55ddec005e8e842920472de08840347
sonu-shivcharan <sonushivcharan@outlook.com>
2026-01-18 17:09:41 +0530

    add starry-night.css

---
+++ app/projects/layout.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../starry-night.css";
+
+function ProjectLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
+
+export default ProjectLayout;
