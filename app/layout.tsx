9922f1d7de46bd0b5a7e9ab3d29ad3512a1d85ab
sonu-shivcharan <sonushivcharan@outlook.com>
2026-05-27 15:43:42 +0530

    add google site verification code

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,6 +59,9 @@ export const metadata: Metadata = {
     index: true,
     follow: true,
   },
+  verification: {
+    google: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION!,
+  },
 };
 
 export default function RootLayout({
